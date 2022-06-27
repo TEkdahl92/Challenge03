cf1455c6ce63add95df3fc785178d774c5263861
TEkdahl92 <106709941+TEkdahl92@users.noreply.github.com>
2022-06-26 22:19:24 -0500

    save changes

--- script.js
+++ script.js
@@ -1,15 +1,53 @@
 // Assignment code here
-
-
+var characterLength = 8;
+var specialCharacter = ['!','?','#','$','&','*','+','%'];
+var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
+var number = ['1','2','3','4','5','6','7','8','9'];
+var choice = [];
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() 
+
+function prompts() {
+    choice = [];
+    characterLength = parseInt(prompt("How many characters would you like to have in your password? (8-128)"));
+
+if (isNaN(characterLength) || characterLength < 8 || characterLength > 128) {    
+    alert("Character length has to be between 8 and 128 characters. Please try again");
+    return false;
+}
+if (confirm("Would you like lowercase letters in your password?")) {
+    choice = choice.concat(lowerCase);
+}
+if (confirm("Would you like Uppercase letters in your password?")) {
+    choice = choice.concat(upperCase);
+}
+if (confirm("Would you like numbers in your password?")) {
+    choice = choice.concat(number);
+}
+if (confirm("Would you like special characters in your password?")) {
+    choice = choice.concat(specialCharacter);
+}
+return true;
+}
+
+
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var correctPrompts = getPrompts();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
+ if (correctPrompts) {
+    var newPassword = generatePassword();
+    passwordText.value = newPassword
+ } else{
+    passwordText.value = "";
+ } 
 
 }
 
